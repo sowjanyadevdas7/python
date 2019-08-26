38ca998cec65ca783a957cb0da394011e49837cc
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2019-08-26 10:33:19 +0530

    Update and rename mathmop.py to swap.py

--- swap.py
+++ swap.py
@@ -9,11 +9,5 @@ print('The value of b after swapping: {}'.format(b))
 output:
   enter a value: 10
 enter b value: 20
-select operation:
-1.add of a and b is:
-2.sub of a and b is:
-3.mult of a and b is:
-4.division of a and b is:
-5.modulus of a and b is:
-enter choice(1/2/3/4/5)1
-10 + 20 = 30
+The value of a after swapping: 20
+The value of b after swapping: 10
