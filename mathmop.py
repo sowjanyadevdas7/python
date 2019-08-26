eca6bd682fd3c0678d4ad4e16488394cc203e0f2
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2019-08-26 10:32:07 +0530

    Update and rename program1-3.py to mathmop.py

---
+++ mathmop.py
@@ -0,0 +1,19 @@
+a = int(input("enter a value:"))
+b = int(input("enter b value:"))
+a = a + b
+b = a - b
+a = a - b
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
+
+output:
+  enter a value: 10
+enter b value: 20
+select operation:
+1.add of a and b is:
+2.sub of a and b is:
+3.mult of a and b is:
+4.division of a and b is:
+5.modulus of a and b is:
+enter choice(1/2/3/4/5)1
+10 + 20 = 30
--- program1-3.py
+++
@@ -1,7 +0,0 @@
-a = int(input("enter a value:"))
-b = int(input("enter b value:"))
-a = a + b
-b = a - b
-a = a - b
-print('The value of a after swapping: {}'.format(a))
-print('The value of b after swapping: {}'.format(b))
