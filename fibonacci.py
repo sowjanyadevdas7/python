d516597836a6ba08eedb8737dffb8839af1c8915
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2019-08-26 10:35:24 +0530

    Update and rename program1-4.py to fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -10,3 +10,18 @@ else:
    print("Fibonacci sequence:")  
    for i in range(nterms):  
        print(recur_fibo(i))  
+output:
+   How many terms? 12
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
