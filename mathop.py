2f45f1f9505f253ebf5dd2d1a6b532339ffb85ab
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2019-08-26 10:34:18 +0530

    Update and rename program1-2.py to mathop.py

--- mathop.py
+++ mathop.py
@@ -30,3 +30,14 @@ elif choice == '5':
 else:
    print("Invalid input")
 
+output:
+    enter a value: 10
+enter b value: 20
+select operation:
+1.add of a and b is:
+2.sub of a and b is:
+3.mult of a and b is:
+4.division of a and b is:
+5.modulus of a and b is:
+enter choice(1/2/3/4/5)1
+10 + 20 = 30
